2ef2b5b4ac2d42f7376e453d7d03c297e0155d2a
adam <financeCoding@gmail.com>
2013-04-01 16:32:55 -0700

    Clean up server logging

--- bin/server.dart
+++ bin/server.dart
@@ -1,7 +1,3 @@
-/*
-Client ID: 327285194570-kbpkgvfe87tlvpue69lf5krdokbepo6j.apps.googleusercontent.com
-Client secret: G6KLdY07klgrDKT2jcAYVE45
- */
 
 import 'dart:io';
 import 'dart:math';
@@ -18,9 +14,33 @@ import "package:google_plus_v1_api/plus_v1_api_console.dart" as plus;
 import "package:google_oauth2_client/google_oauth2_console.dart" as console_auth;
 import "package:logging/logging.dart";
 
-Random random = new Random();
+final String CLIENT_ID = "327285194570-kbpkgvfe87tlvpue69lf5krdokbepo6j.apps.googleusercontent.com";
+final String CLIENT_SECRET = "G6KLdY07klgrDKT2jcAYVE45";
+
+final String TOKENINFO_URL = "https://www.googleapis.com/oauth2/v1/tokeninfo";
+final String TOKEN_ENDPOINT = 'https://accounts.google.com/o/oauth2/token';
+final String TOKEN_REVOKE_ENDPOINT = 'https://accounts.google.com/o/oauth2/revoke';
+
+final Random random = new Random();
+final Logger serverLogger = new Logger("server");
+
+_setupLogger() {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((LogRecord logRecord) {
+    StringBuffer sb = new StringBuffer();
+    sb
+    ..write(logRecord.time.toString())..write(":")
+    ..write(logRecord.loggerName)..write(":")
+    ..write(logRecord.level.name)..write(":")
+    ..write(logRecord.sequenceNumber)..write(": ")
+    ..write(logRecord.message.toString());
+    print(sb.toString());
+  });
+}
 
 class SimpleOAuth2 implements console_auth.OAuth2Console {
+  final Logger logger = new Logger("SimpleOAuth2");
+  
   console_auth.Credentials _credentials;
   console_auth.Credentials get credentials => _credentials;
   void set credentials(value) {
@@ -30,62 +50,83 @@ class SimpleOAuth2 implements console_auth.OAuth2Console {
   console_auth.SystemCache get systemCache => _systemCache;
   
   void clearCredentials(console_auth.SystemCache cache) {
-    print("clearCredentials");
+    logger.fine("clearCredentials(console_auth.SystemCache $cache)");
   }
   
   Future withClient(Future fn(console_auth.Client client)) {
-    //var completer = new Completer();
-    console_auth.Client _httpClient = new console_auth.Client("327285194570-kbpkgvfe87tlvpue69lf5krdokbepo6j.apps.googleusercontent.com",
-        "G6KLdY07klgrDKT2jcAYVE45",
-        _credentials);
-    print("withClient");
+    logger.fine("withClient(Future ${fn}(console_auth.Client client))");
+    console_auth.Client _httpClient = new console_auth.Client(CLIENT_ID, CLIENT_SECRET, _credentials);
     return fn(_httpClient);
   }
   
   void close() {
-    print("close()");
-  }
-  
+    logger.fine("close()");
+  } 
 }
 
 void main() {
+  _setupLogger();
+  
   new Fukiya()
   ..get('/', getIndexHandler)
   ..get('/index.html', getIndexHandler)
   ..get('/index', getIndexHandler)
   ..post('/connect', postConnectDataHandler)
   ..get('/people', getPeopleHandler)
+  ..post('/disconnect', postDisconnectHandler)
   ..staticFiles('./web')
   ..use(new FukiyaJsonParser())
   ..listen('127.0.0.1', 3333);
 }
 
+void postDisconnectHandler(FukiyaContext context) {
+  serverLogger.fine("postDisconnectHandler");
+  
+  String tokenData = context.request.session.containsKey("token") ? context.request.session["token"] : null;
+  if (tokenData == null) {
+    context.response.statusCode = 401;
+    context.send("Current user not connected.");
+    return;
+  }
+  
+  final String revokeTokenUrl = "${TOKEN_REVOKE_ENDPOINT}?token=${tokenData}";
+  context.request.session.remove("token");
+  
+  new http.Client()..get(revokeTokenUrl).then((http.Response response) {
+    serverLogger.fine("GET ${revokeTokenUrl}");
+    serverLogger.fine("Response = ${response.body}");
+    context.send("Successfully disconnected.");
+  });
+}
+
 void getPeopleHandler(FukiyaContext context) {
-  //String tokenData = context.
-  //context.send("getPeopleHandler");
+  serverLogger.fine("getPeopleHandler");
   String accessToken = context.request.session.containsKey("access_token") ? context.request.session["access_token"] : null;
   SimpleOAuth2 simpleOAuth2 = new SimpleOAuth2();
-  console_auth.Credentials creds = new console_auth.Credentials(accessToken);
-  simpleOAuth2.credentials = creds;
+  console_auth.Credentials credentials = new console_auth.Credentials(accessToken);
+  simpleOAuth2.credentials = credentials;
   plus.Plus plusclient = new plus.Plus(simpleOAuth2);
   plusclient.makeAuthRequests = true;
   plusclient.people.list("me", "visible").then((plus.PeopleFeed people) {
-    print("people = $people");
+    serverLogger.fine("/people = $people");
     context.send(people.toString());
   });
 }
 
 void postConnectDataHandler(FukiyaContext context) {
+  serverLogger.fine("postConnectDataHandler");
   String tokenData = context.request.session.containsKey("token") ? context.request.session["token"] : null; // TODO: handle missing token
   String stateToken = context.request.session.containsKey("state_token") ? context.request.session["state_token"] : null;
   String queryStateToken = context.request.queryParameters.containsKey("state_token") ? context.request.queryParameters["state_token"] : null;
   
+  // Check if the token already exists for this session. 
   if (tokenData != null) {
     context.response.statusCode = 400;
     context.send("Current user is already connected.");
     return;
   }
   
+  // Check if any of the needed token values are null or mismatched.
   if (stateToken == null || queryStateToken == null || stateToken != queryStateToken) {
     context.send("POST FAILED tokenData == null || stateToken == null || queryStateToken == null || $stateToken != $queryStateToken"); 
     return;
@@ -99,33 +140,33 @@ void postConnectDataHandler(FukiyaContext context) {
   
   String gPlusId = context.request.queryParameters["gplus_id"];
   StringBuffer sb = new StringBuffer();
+  // Read data from request.
   context.request
   .transform(new StringDecoder())
   .listen((data) => sb.write(data), onDone: () {
-    print(sb.toString());
-    Map m = JSON.parse(sb.toString());
-    String tokenEndpoint = 'https://accounts.google.com/o/oauth2/token';
+    //print(sb.toString());
+    Map requestData = JSON.parse(sb.toString());
+    
     Map fields = {
               "grant_type": "authorization_code",
-              "code": m["code"],
+              "code": requestData["code"],
               // http://www.riskcompletefailure.com/2013/03/postmessage-oauth-20.html
               "redirect_uri": "postmessage",
-              "client_id": "327285194570-kbpkgvfe87tlvpue69lf5krdokbepo6j.apps.googleusercontent.com",
-              "client_secret": "G6KLdY07klgrDKT2jcAYVE45"
+              "client_id": CLIENT_ID,
+              "client_secret": CLIENT_SECRET
     };
     
-    print("trying to post auth code from console");
+    //print("trying to post auth code from console");
     
-    print("fields = $fields");
+    //print("fields = $fields");
     http.Client _httpClient = new http.Client();
-    _httpClient.post(tokenEndpoint, fields: fields).then((http.Response response) {
+    _httpClient.post(TOKEN_ENDPOINT, fields: fields).then((http.Response response) {
       // At this point we have the token and refresh token.
-      
       var credentials = JSON.parse(response.body);
       print("credentials = ${response.body}");
       _httpClient.close();
       
-      var verifyTokenUrl = 'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${credentials["access_token"]}';
+      var verifyTokenUrl = '${TOKENINFO_URL}?access_token=${credentials["access_token"]}';
       new http.Client()
       ..get(verifyTokenUrl).then((http.Response response)  {
         print("response = ${response.body}");
@@ -139,27 +180,30 @@ void postConnectDataHandler(FukiyaContext context) {
         } else {
           context.send("POST FAILED ${userId} != ${gPlusId}"); 
         }
-        
-        //context.send("POST OK"); // TODO: remove this and reaplce with data we want to send back. 
-        // Query the users idenity and store into database.
       });
     });
   });
 }
 
+/**
+ * Sends the client a index file with state token to start the client
+ * side authentication process.
+ */
 void getIndexHandler(FukiyaContext context) {
-  StringBuffer sb = new StringBuffer();
+  serverLogger.fine("getIndexHandler");
+  // Create a state token. 
+  StringBuffer stateTokenBuffer = new StringBuffer();
   new MD5()
   ..add(random.nextDouble().toString().codeUnits)
-  ..close().forEach((s)=>sb.write(s.toRadixString(16)));
-  String stateToken = sb.toString();
+  ..close().forEach((int s) => stateTokenBuffer.write(s.toRadixString(16)));
+  String stateToken = stateTokenBuffer.toString();
   context.request.session["state_token"] = stateToken;
-  context.request.session.forEach((k,v)=>print("[$k: $v]"));
   
+  // Readin the index file and add state token into the meta element. 
   var file = new File("./web/index.html");
-  file.exists().then((exists) {
+  file.exists().then((bool exists) {
     if (exists) {
-      file.readAsString().then((indexDocument) {
+      file.readAsString().then((String indexDocument) {
         Document doc = new Document.html(indexDocument);
         Element metaState = new Element.html('<meta name="state_token" content="${stateToken}">');
         doc.head.children.add(metaState);
@@ -167,7 +211,6 @@ void getIndexHandler(FukiyaContext context) {
         context.response.done.catchError((e) => print("File Response error: ${e}"));
         context.response.close();
       }, onError: (error) => print("error = $error"));
-
     } else {
       context.response.statusCode = 404;
       context.response.close();

9f2e9169a249c688ef05cfaa94db365c2cf2b8f3
adam <financeCoding@gmail.com>
2013-04-02 08:39:23 -0700

    Code clean up and license info

--- server-side/web/quickstart.dart
+++ server-side/web/quickstart.dart
@@ -1,3 +1,40 @@
+/*
+ *
+ * Copyright 2013 Gerwin Sturm
+ * Copyright 2013 Adam Singer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ *
+ * Adapted from Google+ Javascript Quickstart
+ * https://github.com/googleplus/gplus-quickstart-javascript
+ * Copyright 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import "dart:html";
 import "dart:json" as JSON;
 import "package:js/js.dart" as js;
